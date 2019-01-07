613d3bb352c2f3b1e89abf965abe79dce44028c0
alexmatze <alexmatze@web.de>
2019-01-07 13:42:56 +0100

    masking stuff and reverse modeling

---
+++ Scripting/Python/LOFAR/bands/126/masking.py
@@ -0,0 +1,213 @@
+#!/usr/bin/python
+#====================================================
+#This script is used to identify regions
+#in fits images by using modified SEED+GROW algo
+#SEED and GROW procedure iterate until satisfied
+#====================================================
+
+#====================================================
+#          Loading necessary packages
+#====================================================
+
+import astropy.io.fits as pyfits
+import os
+import numpy as np
+from astropy.time import Time
+import pylab as plt
+import scipy
+import scipy.optimize as opt
+import matplotlib.colors as colors
+import matplotlib
+from matplotlib.mlab import bivariate_normal
+
+#====================================================
+#     Loading absolute and temporary variables 
+#====================================================
+
+path_in="band7.fits"
+path_out="mask.fits"
+#=====================================================
+#             Definitions for functions
+#=====================================================
+
+def extrapolate(matrix,y,x): #Function to correct dead pixels by extrapolating from N and NN neighbours
+	matrix[y,x]=(((matrix[y+1,x]+matrix[y,x+1]+matrix[y-1,x]+matrix[y,x-1])/4)*2**0.5+((matrix[y+1,x+1]+matrix[y-1,x+1]+matrix[y+1,x-1]+matrix[y-1,x-1])/4))/(1+2**0.5)
+
+	return matrix
+
+def dist(x, y, xx, yy):
+	x = x * 1.
+	y = y * 1.
+	xx = xx * 1.
+	yy = yy * 1.
+
+	return np.sqrt((yy-y)**2+(xx-x)**2) 
+
+def seed(image,mask,increment,stopval): #Find Seeds to grow where Seed is found as isolated maximum within max and increment but >stopval
+	tmp_mask=mask * 1	#create a temporary mask for region/noregion assoc
+	maxreg=np.max(mask)	
+	canlist=[]		#open empty list for seed candidates
+	for y in range(mask.shape[0]):	#fill temporary mask with 0:no region and 1:any region
+		for x in range(mask.shape[1]):
+			if mask[y,x]==0:
+				tmp_mask[y,x]=1
+			else:
+				tmp_mask[y,x]=0
+	tmp_image= image * tmp_mask #create temporary image with blanked out region pixels
+	maxval=np.max(tmp_image)	#identify remaining maximum
+	if maxval <= stopval:	#if no maximum above limit is found -> no new seed
+		return mask
+        for y in range(mask.shape[0]):	#identify candidate pixels with max-inc<val<max
+                for x in range(mask.shape[1]):
+			if tmp_image[y,x]>maxval-maxval*increment:
+				canlist.append([y,x])
+	tmplst=[]
+	for candidate in canlist:
+		if candidate[0]>1 and candidate[1]>1 and candidate[0]<mask.shape[0]-1 and candidate[1]<mask.shape[1]-1: #drop edge pixel candidates
+			if mask[candidate[0]+1,candidate[1]]==0 and mask[candidate[0],candidate[1]+1]==0 and mask[candidate[0]-1,candidate[1]]==0 and mask[candidate[0],candidate[1]-1]==0:
+				tmplst.append(candidate) #only keep candidates not adjacent to regions
+	canlist=tmplst * 1
+	reg_no=1
+	seedlist=[]
+	#collect connected candidates to regions
+	flr_mask = mask * 0.
+	for can in canlist:
+		flr_mask[can[0],can[1]]=-1
+	for y in range(mask.shape[0]):	#search for candidade pixel and fill adjecient candidates with next no
+		for x in range(mask.shape[1]):
+			if flr_mask[y,x]==-1:
+				if flr_mask[y+1,x] == -1 or flr_mask[y-1,x]==-1 or flr_mask[y,x+1]==-1 or flr_mask[y,x-1]==-1:
+					condition = True
+					chck=[y,x]
+					new_chck=[]
+					tsl=[]
+					while condition:
+						for cent in chck:
+							if flr_mask[cent[0]+1,cent[1]]==-1:
+								new_chck.append([cent[0]+1,cent[1])
+								flr_mask[cent[0]+1,cent[1]]=reg_no
+								tsl.append(cent[0]+1,cent[1]+0)
+							if flr_mask[cent[0]-1,cent[1]]==-1:
+								new_chck.append([cent[0]-1,cent[1])
+								flr_mask[cent[0]-1,cent[1]]=reg_no
+								tsl.append(cent[0]-1,cent[1]+0)
+							if flr_mask[cent[0],cent[1]+1]==-1:
+								new_chck.append([cent[0],cent[1]+1)
+								flr_mask[cent[0],cent[1]+1]=reg_no
+								tsl.append(cent[0]+0,cent[1]+1)
+							if flr_mask[cent[0],cent[1]-1]==-1:
+								new_chck.append([cent[0],cent[1]-1)
+								flr_mask[cent[0],cent[1]-1]=reg_no
+								tsl.append(cent[0]+0,cent[1]-1)
+						chck = new_chck *1
+						if len(new_chck) == 0:
+							seedlist.append(tsl * 1)
+							condition = False
+
+				else:
+					flr_mask[y,x]=reg_no * 1
+					seedlist.append([y,x])
+				
+				reg_no = reg_no +1
+
+	#only consider maximum within each region
+	for seeds in seedlist:
+		maxseed=0.
+		maxx=0
+		maxy=0
+		for seed in seeds:
+			if image(seed[0],seed[1]) > maxseed:
+				maxy=seed[0]
+				maxx=seed[1]
+				maxseed = image(seed[0],seed[1])
+		mask[maxy,maxx]=np.max(mask)+1
+	
+	#write new mask with additional seeds and return mask
+	return mask,maxval
+
+def grow(image,mask,maxval_p,increment,stopval):
+	#do growing procedure
+	grow_condition=True
+	new_mask=mask*1
+	loopcnt=0
+	maxval=maxval_p * 1.
+	while grow_condition:
+		old_mask = mask * 1
+		for y in range(mask.shape[0]):	#scan for existing region pixels and let them grow within parameters
+			for x in range(mask.shape[1]):	
+				if mask[y,x]!=0:
+					if y>0 and x>0 and y<mask.shape[0] and x<mask.shape[1]:
+						if mask[y-1,x]==0 and image[y-1,x]>stopval and image[y-1,x]>maxval*(1-increment):
+							mask[y-1,x]=mask[y,x]
+						if mask[y+1,x]==0 and image[y+1,x]>stopval and image[y+1,x]>maxval*(1-increment):
+							mask[y+1,x]=mask[y,x]
+						if mask[y,x-1]==0 and image[y,x-1]>stopval and image[y,x-1]>maxval*(1-increment):
+							mask[y,x-1]=mask[y,x]
+						if mask[y,x+1]==0 and image[y,x+1]>stopval and image[y,x+1]>maxval*(1-increment):
+							mask[y,x+1]=mask[y,x]
+		if old_mask==mask:
+			blend_mask=mask*1
+			for y in range(mask.shape[0]):
+				for x in range(mask.shape[1]):
+					if mask[y,x]!=0.:
+						blend_mask[y,x]=0.
+					else:
+						blend_mask[y,x]=1.
+			maxvaln=np.max(blend_mask*image)
+			grow_dondition=False
+
+
+	return mask,maxvaln
+			
+	
+
+#=====================================================
+#                      Main
+#=====================================================
+
+
+
+#read in all your file:
+
+hdulist=pyfits.open(path_in) * 1
+dataset=hdulist[0].data
+header=hdulist[0].header
+
+low_level = 0.01
+grow_loops = 10
+init_mask=dataset * 0.
+g_mask = dataset * 0. + 1.
+loop_count = 0
+loop_cond = True
+run_mask=init_mask * 1
+
+
+while loop_cond:
+	print loop_count
+	new_mask,maximumval = seed(dataset,run_mask,0.2,low_level)
+	if new_mask == run_mask:
+		final_mask = new_mask * 1
+		loop_cond = False	
+
+	for i in range(grow_loops):
+		new_mask = grow(dataset,new_mask,maximumval,0.015,low_level)
+
+	loop_count = loop_count + 1
+		
+
+
+
+if os.path.isfile(path_out):
+	os.system("rm " + path_out)
+	
+pyfits.writeto(path_out, final_mask , header)
+
+
+
+
+
+
+
+
+
+
---
+++ Scripting/Python/LOFAR/bands/141/extractcore_mod.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python
+#====================================================
+#This script is used to successfully subtract gaussian
+#components of a fits-file image
+#====================================================
+
+#====================================================
+#          Loading necessary packages
+#====================================================
+
+import astropy.io.fits as pyfits
+import os
+import numpy as np
+from astropy.time import Time
+import pylab as plt
+import scipy
+import scipy.optimize as opt
+import matplotlib.colors as colors
+import matplotlib
+from matplotlib.mlab import bivariate_normal
+
+#====================================================
+#     Loading absolute and temporary variables 
+#====================================================
+
+path_in="band12.fits"
+path_out="modeltest.fits"
+#=====================================================
+#             Definitions for functions
+#=====================================================
+
+def extrapolate(matrix,y,x): #Function to correct dead pixels by extrapolating from N and NN neighbours
+	matrix[y,x]=(((matrix[y+1,x]+matrix[y,x+1]+matrix[y-1,x]+matrix[y,x-1])/4)*2**0.5+((matrix[y+1,x+1]+matrix[y-1,x+1]+matrix[y+1,x-1]+matrix[y-1,x-1])/4))/(1+2**0.5)
+
+	return matrix
+
+def dist(x, y, xx, yy):
+	x = x * 1.
+	y = y * 1.
+	xx = xx * 1.
+	yy = yy * 1.
+
+	return np.sqrt((yy-y)**2+(xx-x)**2) 
+
+def median(a): # a: array of matrices
+	if len(a) == 0:
+		print "ERROR: No Data in Array!!!"
+		return False
+	y_len=np.shape(a[0])[0]
+	x_len=np.shape(a[0])[1]
+
+	for i in range(len(a)):
+		test= (np.shape(a[0]) == np.shape(a[i]))
+		if test == False:
+			print "ERROR: Sizes are different!!!"
+			return False
+
+	c=np.zeros((y_len,x_len))
+	for x in range(x_len):
+		for y in range(y_len):
+			vals=[]
+			for i in range(len(a)):
+				vals.append(a[i][y,x])
+			c[y,x]= np.median(vals) * 1.
+	
+	return c
+
+def twoD_Gaussian((x, y), amplitude, xo, yo, sigma_x, sigma_y, theta, offset):
+    xo = float(xo)
+    yo = float(yo)    
+    a = (np.cos(theta)**2)/(2*sigma_x**2) + (np.sin(theta)**2)/(2*sigma_y**2)
+    b = -(np.sin(2*theta))/(4*sigma_x**2) + (np.sin(2*theta))/(4*sigma_y**2)
+    c = (np.sin(theta)**2)/(2*sigma_x**2) + (np.cos(theta)**2)/(2*sigma_y**2)
+    g = offset + amplitude*np.exp( - (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo) 
+                            + c*((y-yo)**2)))
+    return g.ravel()
+
+def twoD_Gaussian_norav((x, y), amplitude, xo, yo, sigma_x, sigma_y, theta, offset):
+    xo = float(xo)
+    yo = float(yo)    
+    a = (np.cos(theta)**2)/(2*sigma_x**2) + (np.sin(theta)**2)/(2*sigma_y**2)
+    b = -(np.sin(2*theta))/(4*sigma_x**2) + (np.sin(2*theta))/(4*sigma_y**2)
+    c = (np.sin(theta)**2)/(2*sigma_x**2) + (np.cos(theta)**2)/(2*sigma_y**2)
+    g = offset + amplitude*np.exp( - (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo) 
+                            + c*((y-yo)**2)))
+    return g
+
+
+
+#=====================================================
+#                      Main
+#=====================================================
+
+#read in all your file:
+
+hdulist=pyfits.open(path_in) * 1
+dataset=hdulist[0].data
+header=hdulist[0].header
+
+dataset_norav=dataset * 1
+
+datasetr=dataset * 1
+dataset=dataset.ravel()
+
+x_size=header["NAXIS1"]
+y_size=header["NAXIS2"]
+
+### Parameters for Modelcomponent
+x_max=127 #x position of peak
+y_max=128 #y position of peak
+
+thet=header["BPA"] #position angle of 2d gauss in deg
+FWHM1=header["BMAJ"] #fwhm of first 1d gauss in deg
+FWHM2=header["BMIN"] #fwhm of second 1d gauss in deg
+degperpix=-1*header["CDELT1"]#degree per pixel
+tval=0 #Value offset for fit (noise)
+amp=1.99264 #Amplitude of max position
+#-------------------------------------
+#apply corrections to meet present definitions
+#thet=thet - 90. #correct position angle for needed definition
+FWHM1=FWHM1/degperpix #converse FWHM1 from deg to pix
+FWHM2=FWHM2/degperpix #converse FWHM2 from deg to pix
+sigm1=FWHM1/2.355 #converse from FWHM1 to 1sigma
+sigm2=FWHM2/2.355 #converse from FWHM2 to 1sigma
+#build grid
+thet=(thet/180.)*scipy.pi #change deg to rad
+
+x=np.linspace(0,x_size-1,x_size)
+y=np.linspace(0,y_size-1,y_size)
+x, y=np.meshgrid(x, y)
+
+modelimg=twoD_Gaussian_norav((x,y),amp,x_max,y_max,sigm1,sigm2,thet,tval)
+
+plt.figure("Modelcomponent"); plt.imshow(modelimg,norm=matplotlib.colors.LogNorm());plt.colorbar();plt.gca().invert_yaxis();plt.show()
+
+resid=dataset_norav - modelimg
+
+if os.path.isfile(path_out):
+	os.system("rm " + path_out)
+	
+pyfits.writeto(path_out, resid, header)
+
+
+
+
+
+
+
+
+
+
